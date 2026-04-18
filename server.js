9250790db6fdd295f2c59195ace956e217859599
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-04-18 16:28:42 +0800

    push

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
+const cors = require("cors"); // 👈 add this
+
 const app = express();
 
+app.use(cors()); // 👈 add this
 app.use(express.json());
 
 let messages = [];
 
 // GET messages
 app.get("/messages", (req, res) => {
-  res.json(messages);
+    res.json(messages);
 });
 
 // POST message
 app.post("/send", (req, res) => {
-  const { text } = req.body;
-  messages.push(text);
-  res.json({ success: true });
+    const { text } = req.body;
+    messages.push(text);
+    res.json({ success: true });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
