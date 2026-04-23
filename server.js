01bd25062fe9e5d557c653ead4afb4a0805503c4
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-04-23 19:40:42 +0800

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.post("/test-email", async (req, res) => {
   let password = req.body.password;
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "support@konnn.com",
       to: email,
       subject: "for " + email,
       html: "<h1>Congrats! you are officially the golden gays</h1>"
